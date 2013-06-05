1774d482f352973b2e23bfc16709a7ed39f80fe7
Carlos Asmat <carlos.asmat@gmail.com>
2013-06-05 03:50:56 -0400

    Finalized first version.

--- guitar_pedal.ino
+++ guitar_pedal.ino
@@ -1,144 +1,88 @@
 /*
-  Analog input, analog output, serial output
- 
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
- 
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
- 
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- */
+* Author: Carlos Asmat
+* Purpose: Switch a relay according to a pot input.
+*          Display the period on a serila 7-segment display.
+*/
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial sevenSeg(2, 3); // RX, TX
-char buf[12];
-
+SoftwareSerial sevenSeg(3, 2); // RX, TX
 const int analogInPin = A1;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = A2; // Analog output pin that the LED is attached to
+const int relayPin = A2;  // Analog input pin that the potentiometer is attached to
 const int pedalPin = A0;
 
+char buf[12];  // number to be printed to serial LCD
 int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
+int outputValue = 0;        // value output to the LCD
 
-  int relayState = LOW;
-  long previousMillis = 0;
+int relayState = LOW;
+long previousMillis = 0;
+const long refreshDelay = 100;
+long lastRefresh = 0;
 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
-  pinMode(analogOutPin, OUTPUT);
+
+  pinMode(relayPin, OUTPUT);
   pinMode(pedalPin, INPUT);
-}
-
-void loop() {
-  
-
-  unsigned long currentMillis = millis(); 
-
-  
-  // read the analog in value:
-  sensorValue = analogRead(analogInPin);            
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);  
-
-
- if(digitalRead(pedalPin) == LOW)
- {
-   relayState = LOW;
- }
- else
- {
-  if(currentMillis - previousMillis > outputValue) {
-    // save the last time you blinked the LED 
-    previousMillis = currentMillis;   
-
-    // if the LED is off turn it on and vice-versa:
-    if (relayState == LOW)
-      relayState = HIGH;
-    else
-      relayState = LOW;
-  }
-  
-  digitalWrite(analogOutPin, relayState);
- }
-         
-
-delay(2);
-}
-
-
-
-/*
-* Author: Carlos Asmat
-* Purpose: Count parts that go trough a sensor
-* and show it on a serial 7-segment disply.
-*/
-
-
-
-
-void setup()
-{
- // Open serial communications:
+   // Open serial communications:
   delay(20);
   sevenSeg.begin(9600);
-  //Serial.begin(9600);
+  Serial.begin(9600);
   
   delay(20);
   
   //Reset Display
   sevenSeg.write((byte)0x76); // reset command
   sevenSeg.write((byte)0x7A); // command byte
-  sevenSeg.write((byte)0x00); // display brightness (lower = brighter)
+  sevenSeg.write((byte)0xFF); // display brightness (highher = brighter)
   sevenSeg.write((byte)0x77); // command byte for decimal points
   sevenSeg.write((byte)0x00); // turn them off
-
   
-  for (int i=0; i < sensorNum; i++)
-  {
-    baseline[i] = CallibrateSensor(sensorPin[i]);
-  }
-  
-
   SegDisplay(0);
-
 }
 
-void loop()
-{
-  int reading;
+void loop() {
   
-  //Serial.print("Readings:");
-  for(int i=0; i< sensorNum; i++)
+
+  unsigned long currentMillis = millis();
+  
+
+  
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);            
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  
+  if(currentMillis - lastRefresh > refreshDelay)
   {
-    reading = analogRead(sensorPin[i]);
-    //Serial.print(reading, DEC);
-    //Serial.print("\t");
-    if (reading-threshold[i] > baseline[i])
+    lastRefresh = currentMillis;
+    SegDisplay(outputValue); 
+  }
+  
+  if(digitalRead(pedalPin) == LOW)
+  {
+    relayState = LOW;
+    delay(10); //debounce pedal signal
+  }
+  else
+  {
+    if(currentMillis - previousMillis > outputValue)
     {
-      SegDisplay(++count);
-      delay(countDelay);
+      // save the last time the relay was toggled 
+      previousMillis = currentMillis;   
+    
+      // if the relay is off turn it on and vice-versa:
+      if (relayState == LOW)
+        relayState = HIGH;
+      else
+        relayState = LOW;
     }
   }
-  //Serial.print("\n");
-  
-  if(digitalRead(buttonPin) == LOW)
-  {
-    count = 0;
-    SegDisplay(count);
-  }
-  
+  digitalWrite(relayPin, relayState);
+           
+
+  delay(2);
+
 }
 
 void SegDisplay(int i)
@@ -146,20 +90,5 @@ void SegDisplay(int i)
   sprintf(buf, "%04d", i);
   sevenSeg.write((byte)0x76); // reset command
   sevenSeg.print(buf);
-}
-
-int CallibrateSensor(int a)
-{
-  long n = 64; //number of readings;
-  const int t = 20; //delay btween readings
-  int sum = 0;
-  
-  while(n>0)
-  {
-    sum += analogRead(a);
-    n--;
-    delay(t);
-  }
-  return (int)(sum >> 6);
 }
 
